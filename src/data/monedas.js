70bb00a1d1a563d4d2c64d237ac8201e466061b1
wacho91 <criskol.71@gmail.com>
2022-11-09 21:12:00 -0500

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar EstadoUnidense'},
+    {id: 'COP', nombre: 'Peso Colombiano'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'},
+    {id: 'MXN', nombre: 'Peso Mexicano'}
+]
+
+export {
+    monedas
+}
---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled'
+import {useState} from 'react'
+
+const LabelSelect = styled.label`
+    color: #FFF;
+    display: block;
+    font-family: 'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`
+
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 14px;
+    border-radius: 10px;
+
+`
+
+const useSelectMonedas = (label, opciones) => {
+
+    const [state, setState] = useState('');
+  
+    const SelectMonedas = () => (
+        <>
+            <LabelSelect>{label}</LabelSelect>
+            <Select
+                value={state}
+                onChange={e => setState(e.target.value)}
+            >
+                <option value="">Seleccione</option>
+                {opciones.map(opcion => (
+                    <option 
+                        key={opcion.id}
+                        value={opcion.id}
+                    >{opcion.nombre}</option>
+                ))}
+            </Select>
+        </>
+    )
+
+    // const SelectCriptoMoneda = (label) => {
+    //     <>
+    //         <LabelSelect>{label}</LabelSelect>
+    //     </>
+    // }
+
+    return [state, SelectMonedas]
+}
+
+export default useSelectMonedas
